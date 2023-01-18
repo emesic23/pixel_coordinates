fe97629a1dbe2b8fe94045112d0a044325767aac
emesic23 <esmirmesic23@gmail.com>
2023-01-18 13:13:20 -0800

    fixed distortion bug

--- rectify.py
+++ rectify.py
@@ -4,10 +4,10 @@ from matplotlib import pyplot as plt
 
 camera_mtx = np.array([[511.03573247, 0.000000, 311.80346835], [0.000000, 508.22913692, 261.56701122], [0.000000, 0.000000, 1.000000]])
 R = np.array([[1.000000, 0.000000, 0.000000], [0.000000, 1.000000, 0.000000], [0.000000, 0.000000, 1.000000]])
-
+D = np.array([-0.43339599, 0.18974767, -0.00146426, 0.00118333, 0.000000])
 image = cv2.imread("sample/thermal-25080.tiff")
 height, width, _ = image.shape
-map1,map2=cv2.initUndistortRectifyMap(camera_mtx, None, None, None, (width, height), cv2.CV_32FC1)
+map1,map2=cv2.initUndistortRectifyMap(camera_mtx, D, None, None, (width, height), cv2.CV_32FC1)
 
 mapped = cv2.remap(image, map1, map2, cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0, 0))
 
